50450ad624a169e6e00ba93233fc65b3bfa2c6d6
me <xyhe6@iflytek.com>
2018-12-18 17:35:00 +0800

    first

---
+++ src/main/java/com/pilgrim/hxy/HxyApplication.java
@@ -0,0 +1,14 @@
+package com.pilgrim.hxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HxyApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/pilgrim/hxy/common/Test.java
@@ -0,0 +1,8 @@
+package com.pilgrim.hxy.common;
+
+/**
+ * @author hexy
+ * @date 2018/12/1816:02
+ */
+public class Test {
+}
